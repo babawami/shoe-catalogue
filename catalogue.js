bb772e00d3a465e5de0c0176f76facb85e2fe3b2
Andrew <andrew.monamodi@gmail.com>
2018-06-14 10:16:07 +0200

    first commit

---
+++ catalogue.js
@@ -0,0 +1,80 @@
+function ShoeFinder(){
+  let shoeData = [
+        {color : 'blue',brand : "Nike",  price : 350, size: 7,in_stock : 5 },
+        {color : 'white',  brand : "Adidas",  price : 275,size: 10,  in_stock : 3},
+        { color : 'brown',  brand : "Adidas",  price : 350,  size: 7,in_stock : 20},
+        {color : 'blue',  brand : "Reebok",  price : 450,size: 6,in_stock : 15  },
+        {color : 'black',  brand : "Puma",price : 275,size: 9,in_stock : 3},
+];
+
+ function shoeMatch(chosenBrand,chosenSize,chosenColour){
+   const shoeArr = [];
+   for(var i = 0; i< shoeData.length; i++){
+     let listObj = shoeData[i];
+     // console.log(listObj);
+     if(chosenBrand ==listObj.brand && chosenSize == listObj.size && chosenColour == listObj.color ){
+       shoeArr.push(listObj)
+     }
+
+     if(chosenBrand === listObj.brand && chosenSize === listObj.size && chosenColour === ''){
+       shoeArr.push(listObj);
+     }
+     if(chosenBrand === listObj.brand && chosenSize === '' && chosenColour === listObj.color){
+       shoeArr.push(listObj);
+     }
+     if(chosenBrand === '' && chosenSize === listObj.size && chosenColour === listObj.color){
+       shoeArr.push(listObj);
+     }
+     if(chosenBrand === listObj.brand && chosenSize === ''&& chosenColour === ''){
+       shoeArr.push(listObj);
+     }
+     if(chosenBrand === '' && chosenSize === listObj.size && chosenColour === ''){
+       shoeArr.push(listObj);
+     }
+     if(chosenBrand === '' && chosenSize === '' && chosenColour === listObj.color){
+       shoeArr.push(listObj);
+     }
+
+
+   }
+      // console.log(shoeMatch('Adidas'));
+   return shoeArr;
+
+
+   //  shoeData.forEach(function(listObj) {
+   // console.log(listObj)
+   //
+   // })
+
+
+
+
+
+
+
+
+
+// function matchingShoe(brand, color, size ){
+//   for ( var i = 0 ; i< shoeData.length; i++){
+//      var shoeList = shoeData[i]; // loops through the array returns objects
+//
+//
+//
+//   // var exists = false
+//   // if(shoeList.color == color && shoeList.brand == brand && shoeList.size == size){
+//   //   shoelist.in_stock ++
+//   //   exists = true
+//   // }
+//
+//   }
+//    //return shoeList;
+//
+//
+}
+
+return {
+// matchingShoe: matchingShoe,
+shoeMatch,
+
+}
+}
---
+++ test/test.js
@@ -0,0 +1,85 @@
+describe(' Shoe Catalogue ', function() {
+  it("Return Shoe/shoes that match the brand selected", function() {
+    let shoes = ShoeFinder();
+    console.log(shoes.shoeMatch('Adidas', '', ''));
+    assert.deepEqual(shoes.shoeMatch('Adidas', '', ''), [{
+      color: 'white',
+      brand: "Adidas",
+      price: 275,
+      size: 10,
+      in_stock: 3
+    }, {
+      color: 'brown',
+      brand: "Adidas",
+      price: 350,
+      size: 7,
+      in_stock: 20
+    }, ]);
+  });
+  it("Return Shoe/shoes that match the size selected", function() {
+    let shoes = ShoeFinder();
+    assert.deepEqual(shoes.shoeMatch('', 7, ''), [{
+      color: 'blue',
+      brand: "Nike",
+      price: 350,
+      size: 7,
+      in_stock: 5,
+    }, {
+      color: 'brown',
+      brand: "Adidas",
+      price: 350,
+      size: 7,
+      in_stock: 20
+    }, ]);
+  });
+  it("Return Shoe/shoes that match the colour selected", function() {
+    let shoes = ShoeFinder();
+    assert.deepEqual(shoes.shoeMatch('', '', 'blue'), [{
+      color: 'blue',
+      brand: "Nike",
+      price: 350,
+      size: 7,
+      in_stock: 5,
+    }, {
+      color: 'blue',
+      brand: "Reebok",
+      price: 450,
+      size: 6,
+      in_stock: 15
+    }, ]);
+  });
+  it("Return Shoe/shoes that match the size and brand selected", function() {
+    let shoes = ShoeFinder();
+    assert.deepEqual(shoes.shoeMatch('Nike', 7, ''), [{
+      color: 'blue',
+      brand: "Nike",
+      price: 350,
+      size: 7,
+      in_stock: 5,
+    }]);
+  });
+  it("Return Shoe/shoes that match the size and colour selected", function() {
+    let shoes = ShoeFinder();
+    assert.deepEqual(shoes.shoeMatch('',9,'black'), [{
+      color: 'black',
+      brand: 'Puma',
+      price: 275,
+      size: 9,
+      in_stock: 3,
+    }
+   ]);
+  });
+  it("Return Shoe/shoes that match the brand and colour selected", function() {
+    let shoes = ShoeFinder();
+    assert.deepEqual(shoes.shoeMatch('Adidas','','white'), [{
+      color: 'white',
+      brand: 'Adidas',
+      price: 275,
+      size: 10,
+      in_stock: 3,
+    }
+   ]);
+  });
+
+
+});
