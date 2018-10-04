ed8d288a1c74e7c175e6e14264b736f698c01ec1
Andrew <andrew.monamodi@gmail.com>
2018-10-04 17:17:04 +0200

    doing tests for my functions

--- catalogue.js
+++ catalogue.js
@@ -1,47 +1,28 @@
 function ShoeFinder(){
   let shoeData = [
-        {color : 'blue',brand : "Nike",  price : 350, size: 7,in_stock : 5 },
-        {color : 'white',  brand : "Adidas",  price : 275,size: 10,  in_stock : 3},
-        { color : 'brown',  brand : "Adidas",  price : 350,  size: 7,in_stock : 20},
-        {color : 'blue',  brand : "Reebok",  price : 450,size: 6,in_stock : 15  },
-        {color : 'black',  brand : "Puma",price : 275,size: 9,in_stock : 3},
+        {color : 'blue',brand : "nike",  price : 350, size: 7,in_stock : 5 },
+        {color : 'white',  brand : "adidas",  price : 275,size: 10,  in_stock : 3},
+        { color : 'brown',  brand : "adidas",  price : 350,  size: 7,in_stock : 20},
+        {color : 'blue',  brand : "reebok",  price : 450,size: 6,in_stock : 15  },
+        {color : 'black',  brand : "puma",price : 275,size: 9,in_stock : 3},
 ];
 
-const shoeArr = [];
+let shoeArr = [];
 
- function shoeMatch(chosenBrand,chosenSize,chosenColour){
-   for(var i = 0; i< shoeData.length; i++){
-     let listObj = shoeData[i];
-     // console.log(listObj);
-     if(chosenBrand ==listObj.brand && chosenSize == listObj.size && chosenColour == listObj.color ){
-       shoeArr.push(listObj)
-     }
-     else if(chosenBrand === listObj.brand && chosenSize === listObj.size && chosenColour === ''){
-       shoeArr.push(listObj);
-     }
-
-     else if(chosenBrand === listObj.brand && chosenSize === '' && chosenColour === listObj.color){
-       shoeArr.push(listObj);
-     }
-    else if(chosenBrand === '' && chosenSize === listObj.size && chosenColour === listObj.color){
-       shoeArr.push(listObj);
-     }
-    else if(chosenBrand === listObj.brand && chosenSize === ''&& chosenColour === ''){
-       shoeArr.push(listObj);
-     }
-  else   if(chosenBrand === '' && chosenSize === listObj.size && chosenColour === ''){
-       shoeArr.push(listObj);
-     }
-    else if(chosenBrand === '' && chosenSize === '' && chosenColour === listObj.color){
-       shoeArr.push(listObj);
-     }
-
-
-   }
-      // console.log(shoeMatch('Adidas'));
-   return shoeArr;
-   }
-
+//define a filtering function
+function shoeMatch(chosenColour, chosenBrand, chosenSize){
+  //loop through the list
+    shoeData.forEach(function(currentData){
+      if(chosenColour == currentData.color || chosenColour === ''){
+        if(chosenBrand == currentData.brand || chosenBrand === ''){
+          if( chosenSize == currentData.size || chosenSize == null){
+            shoeArr.push(currentData);
+          }
+        }
+      }
+    });
+    return shoeArr;
+  }
 
 function errorMessage(){
   if( shoeArr.length === 0){
@@ -49,35 +30,41 @@ function errorMessage(){
   }
 }
 
+// add new stock 
+ function addStock(newColor,newBrand,newSize,newPrice,newStock) {
+  let product_item = false;
+  for(let i=0; i <shoeData.length; i++){
+    let currentData = shoeData[i];
+    //When  product item is true udate shoe stock
+    if(newColor == currentData.color && newBrand == currentData.brand  && newSize == currentData.size ){
+      currentData.in_stock += newStock;
+      currentData.price = newPrice;
+      product_item = true;
+      // return "inventory updated"
+    }
+    // console.log(shoeData)
+   
+  }
+  //If product item is false push new shoe to shoe shoeData
+  if (!product_item) {
+    shoeData.push({
+      color: newColor,
+      brand: newBrand,
+      price: newPrice,
+      size: newSize,
+      in_stock: newStock
+    });
+  }
+  console.log(shoeData)
+  return shoeData;
+};
 
-
-
-
-
-
-
-
-// function matchingShoe(brand, color, size ){
-//   for ( var i = 0 ; i< shoeData.length; i++){
-//      var shoeList = shoeData[i]; // loops through the array returns objects
-//
-//
-//
-//   // var exists = false
-//   // if(shoeList.color == color && shoeList.brand == brand && shoeList.size == size){
-//   //   shoelist.in_stock ++
-//   //   exists = true
-//   // }
-//
-//   }
-//    //return shoeList;
-//
-//
 
 
 return {
 shoeMatch,
-errorMessage
+errorMessage,
+addStock
 
 }
 }
--- test/test.js
+++ test/test.js
@@ -5,60 +5,40 @@ describe(' Shoe Catalogue ', function() {
     // console.log(shoes.errorMessage());
     assert.deepEqual(shoes.errorMessage(),'No Shoe Found' );
   });
-  it("Return Shoe/shoes that match the brand selected", function() {
+  it("Return Shoe/shoes that match the color selected", function() {
     let shoes = ShoeFinder();
     // console.log(shoes.shoeMatch('Adidas', 7, 'black'));
-    assert.deepEqual(shoes.shoeMatch('Adidas', '', ''), [{
-      color: 'white',
-      brand: "Adidas",
-      price: 275,
-      size: 10,
-      in_stock: 3
-    }, {
-      color: 'brown',
-      brand: "Adidas",
-      price: 350,
-      size: 7,
-      in_stock: 20
-    }, ]);
+    assert.deepEqual(shoes.shoeMatch('white', '', null), [
+      {color: 'white',brand: "adidas",price: 275,size: 10,in_stock: 3}
+    ]);
   });
   it("Return Shoe/shoes that match the size selected", function() {
     let shoes = ShoeFinder();
-    assert.deepEqual(shoes.shoeMatch('', 7, ''), [{
+    assert.deepEqual(shoes.shoeMatch('', '', 7), [{
       color: 'blue',
-      brand: "Nike",
+      brand: "nike",
       price: 350,
       size: 7,
       in_stock: 5,
     }, {
       color: 'brown',
-      brand: "Adidas",
+      brand: "adidas",
       price: 350,
       size: 7,
       in_stock: 20
     }, ]);
   });
-  it("Return Shoe/shoes that match the colour selected", function() {
+  it("Return Shoe/shoes that match the brand selected", function() {
     let shoes = ShoeFinder();
-    assert.deepEqual(shoes.shoeMatch('', '', 'blue'), [{
-      color: 'blue',
-      brand: "Nike",
-      price: 350,
-      size: 7,
-      in_stock: 5,
-    }, {
-      color: 'blue',
-      brand: "Reebok",
-      price: 450,
-      size: 6,
-      in_stock: 15
-    }, ]);
+    assert.deepEqual(shoes.shoeMatch('', 'puma', null), [
+    {color: "black", brand: "puma", price: 275, size: 9, in_stock: 3}
+     ]);
   });
   it("Return Shoe/shoes that match the size and brand selected", function() {
     let shoes = ShoeFinder();
-    assert.deepEqual(shoes.shoeMatch('Nike', 7, ''), [{
+    assert.deepEqual(shoes.shoeMatch('','nike' , 7), [{
       color: 'blue',
-      brand: "Nike",
+      brand: "nike",
       price: 350,
       size: 7,
       in_stock: 5,
@@ -66,9 +46,9 @@ describe(' Shoe Catalogue ', function() {
   });
   it("Return Shoe/shoes that match the size and colour selected", function() {
     let shoes = ShoeFinder();
-    assert.deepEqual(shoes.shoeMatch('',9,'black'), [{
+    assert.deepEqual(shoes.shoeMatch('black','',9), [{
       color: 'black',
-      brand: 'Puma',
+      brand: 'puma',
       price: 275,
       size: 9,
       in_stock: 3,
@@ -77,9 +57,9 @@ describe(' Shoe Catalogue ', function() {
   });
   it("Return Shoe/shoes that match the brand and colour selected", function() {
     let shoes = ShoeFinder();
-    assert.deepEqual(shoes.shoeMatch('Adidas','','white'), [{
+    assert.deepEqual(shoes.shoeMatch('white','adidas',null), [{
       color: 'white',
-      brand: 'Adidas',
+      brand: 'adidas',
       price: 275,
       size: 10,
       in_stock: 3,
@@ -88,9 +68,9 @@ describe(' Shoe Catalogue ', function() {
   });
   it("Return Shoe/shoes that match the brand,colour and size selected", function() {
     let shoes = ShoeFinder();
-    assert.deepEqual(shoes.shoeMatch('Adidas',10,'white'), [{
+    assert.deepEqual(shoes.shoeMatch('white','adidas',10), [{
       color: 'white',
-      brand: 'Adidas',
+      brand: 'adidas',
       price: 275,
       size: 10,
       in_stock: 3,
@@ -101,6 +81,28 @@ describe(' Shoe Catalogue ', function() {
     let shoes = ShoeFinder();
     assert.deepEqual(shoes.shoeMatch('Adidas','','black'), []);
   });
+  it("increase stock quantity when new shoe is added", function() {
+    let shoes = ShoeFinder();
+    assert.deepEqual(shoes.addStock('white','adidas',10,280,10), [
+      {color: "blue", brand: "nike", price: 350, size: 7, in_stock: 5},
+      {color: "white", brand: "adidas", price: 280, size: 10, in_stock: 13},
+      {color: "brown", brand: "adidas", price: 350, size: 7, in_stock: 20},
+      {color: "blue", brand: "reebok", price: 450, size: 6, in_stock: 15},
+      {color: "black", brand: "puma", price: 275, size: 9, in_stock: 3}]);
+  });
+  it("Add new inventory when shoe is not in stock", function() {
+    let shoes = ShoeFinder();
+    assert.deepEqual(shoes.addStock('pink','fila',6,200,20), [
+    {color: "blue", brand: "nike", price: 350, size: 7, in_stock: 5},
+    {color: "white", brand: "adidas", price: 275, size: 10, in_stock: 3}, 
+    {color: "brown", brand: "adidas", price: 350, size: 7, in_stock: 20},
+    {color: "blue", brand: "reebok", price: 450, size: 6, in_stock: 15},
+    {color: "black", brand: "puma", price: 275, size: 9, in_stock: 3},
+    {color: "pink", brand: "fila", price: 200, size: 6, in_stock: 20}]);
+  });
+
+
+
 
 
 });
